dac76990fb8251cd563058dec420981521a875b7
서민재 <seominjae@seominjaeui-MacBookPro.local>
2024-05-03 14:58:14 +0900

    ⭐ feat: Mockito로 Stubbing 하기 까지 수강 & 실습 완료

--- cafekiosk/src/main/java/sample/cafekiosk/spring/domain/order/Order.java
+++ cafekiosk/src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.domain.order;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.BaseEntity;
@@ -33,19 +34,26 @@ public class Order extends BaseEntity {
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderProduct> orderProducts = new ArrayList<>();
 
-    public Order(List<Product> products, LocalDateTime registeredDateTime) {
-        this.orderStatus = OrderStatus.INIT;
+
+    @Builder
+    private Order(List<Product> products, OrderStatus status, LocalDateTime registeredDateTime) {
+        this.orderStatus = status;
         this.totalPrice = calculateTotalPrice(products);
         this.registeredDateTime = registeredDateTime;
         this.orderProducts = products.stream()
                 .map(product -> new OrderProduct(this, product))
-                .collect(Collectors.toList());
+                .toList();
     }
 
 
 
     public static Order create(List<Product> products, LocalDateTime registeredDateTime) {
-        return new Order(products, registeredDateTime);
+        return Order.builder()
+                .status(OrderStatus.INIT)
+                .products(products)
+                .registeredDateTime(registeredDateTime)
+                .build();
+
     }
 
     private int calculateTotalPrice(List<Product> products) {
